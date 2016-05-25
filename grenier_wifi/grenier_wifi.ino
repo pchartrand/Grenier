a684c3e68ae0cda0dbae5780c00e4340c3033d98
Philippe Chartrand <philippe.chartrand@videotron.ca>
2016-05-24 20:56:59 -0400

    change output pins, change voltage threshold, increase buffer for elapsed time display

--- grenier_wifi/grenier_wifi.ino
+++ grenier_wifi/grenier_wifi.ino
@@ -6,11 +6,11 @@ const char myPSK[] = "fafabebe1a1a";
 
 /* digital outputs */
 //pins 8 and 9 are used for serial communication with the esp8266
-#define SOLAR_1_OUT 2
-#define SOLAR_2_OUT 3
-#define FAN_1_OUT 10 //set to high if temp > high
-#define FAN_2_OUT 11 //set to high if temp > high
-#define FAN_3_OUT 12 //set to high if solar is strong 
+#define SOLAR_1_OUT 3
+#define SOLAR_2_OUT 10
+#define FAN_1_OUT 11 //set to high if temp > high
+#define FAN_2_OUT 12 //set to high if temp > high
+#define FAN_3_OUT 2 //set to high if solar is strong 
 
 /* analog inputs */
 #define TEMP_1_IN 0
@@ -18,10 +18,13 @@ const char myPSK[] = "fafabebe1a1a";
 #define VOLTAGE_1_IN 2
 #define VOLTAGE_2_IN 3
 
+/* threshold temperatures */
+const float hot = 23.0;
+const float extraHot = 27.0;
 
 /* reference voltages */
 const int referenceVolts = 5;
-const float charged = 12.0;
+const float charged = 15.0;
 
 /* resistor divider network for voltage measurement */
 const float voltageDivider = 10.0;
@@ -30,10 +33,6 @@ const float voltageDivider = 10.0;
 const int measurementTime = 100; 
 const unsigned long sleepTime = 60000;
 
-/* threshold temperatures */
-const float hot = 24.0;
-const float extra_hot = 30.0;
-
 /* NTC polynomial coefficients for voltage to temperature conversion */
 const float a3 = 3.296;
 const float b2 = -22.378;
@@ -41,14 +40,17 @@ const float c1 =  70.951;
 const float d = -49.382;
 
 /* global variables */
-unsigned long start_time;
+unsigned long startTime;
 unsigned long time;
-unsigned long leap_time;
+unsigned long leapTime;
 unsigned long elapsed = 0;
-int source_one_status;
-int source_two_status;
-int fan_status;
-int extra_fan_status;
+
+long fanOnTime;
+
+int sourceOneStatus;
+int sourceTwoStatus;
+int fanStatus = LOW;
+int extraFanStatus;
 
 float temp1;
 float temp2;
@@ -75,7 +77,7 @@ void serverSetup()
 }
 
 void setup(){
-  start_time = millis();
+  startTime = millis();
   pinMode(SOLAR_1_OUT, OUTPUT);
   pinMode(SOLAR_2_OUT, OUTPUT);  
   pinMode(FAN_1_OUT, OUTPUT);
@@ -93,15 +95,18 @@ void loop(){
   temp2 = readTemp(TEMP_2_IN);
   volt1 = readVoltage(VOLTAGE_1_IN);
   volt2 = readVoltage(VOLTAGE_2_IN);
-  leap_time = millis();
-  if (leap_time > time + sleepTime){
-    source_one_status = setPowerSource(volt1, SOLAR_1_OUT);
-    source_two_status = setPowerSource(volt2, SOLAR_2_OUT);
-    fan_status = controlFans(temp1, temp2);
-    extra_fan_status = controlExtraFan(temp1, temp2);
-    time = leap_time;
+  leapTime = millis();
+  if (leapTime > time + sleepTime){
+    sourceOneStatus = setPowerSource(volt1, SOLAR_1_OUT);
+    sourceTwoStatus = setPowerSource(volt2, SOLAR_2_OUT);
+    if (fanStatus == HIGH){
+         fanOnTime += (leapTime - time);
+    }
+    fanStatus = controlFans(temp1, temp2);
+    extraFanStatus = controlExtraFan(temp1, temp2);
+    time = leapTime;
   }
-  elapsed = leap_time - start_time;
+  elapsed = leapTime - startTime;
   ESP8266Client client = server.available(500);
   if (client) 
   {
@@ -122,23 +127,33 @@ void loop(){
           // send a standard http response header:
           client.print(htmlHeader);
           String htmlBody;
-          htmlBody = F("<html>\n<head><title>Grenier</title></head>\n<body><h1>Grenier</h1>\n<p>température 1 :");
+          htmlBody  = F("<html>\n<head><title>Grenier</title></head>\n<body><h1>Grenier</h1>\n<p>Température intérieure :");
           htmlBody += temp1;
-          htmlBody += F("C\n<br />température 2 :");
+          htmlBody += F("C\n<br />Température extérieure :");
           htmlBody += temp2;
-          htmlBody += F("C\n</p>\n<p>panneau solaire 1 : ");
+          htmlBody += F("C\n</p>\n<p>Tension sur panneau solaire 1 : ");
           htmlBody += volt1;
-          htmlBody += F("V\n<br />panneau solaire 2 : ");
+          htmlBody += F("V\n<br />Tension sur panneau solaire 2 : ");
           htmlBody += volt2;
           htmlBody += F("V</p>\n");
           client.print(htmlBody);
-          htmlBody = F("<p>ventilateurs 1 et 2 en fonction : ");
-          htmlBody += booleanToText(fan_status);
-          htmlBody += F("<br />\nventilateur supplémentaire en fonction : ");
-          htmlBody += booleanToText(extra_fan_status);
-          htmlBody += F("</p>\n<p>arduino en fonction depuis : ");
+          
+          htmlBody  = F("<p>Ventilateur 1 sur solaire : ");
+          htmlBody += booleanToText(sourceOneStatus);
+          htmlBody += F("<br />\nVentilateur 2 sur solaire : ");
+          htmlBody += booleanToText(sourceTwoStatus);
+          htmlBody += F("</p>\n<p>ventilateurs 1 et 2 en fonction : ");
+          htmlBody += booleanToText(fanStatus);
+          htmlBody += F("<br />\nVentilateur supplémentaire en fonction : ");
+          htmlBody += booleanToText(extraFanStatus);
+          htmlBody += F("</p>\n");
+          client.print(htmlBody);
+          
+          htmlBody  = F("<p>Arduino en fonction depuis : ");
           htmlBody += secondsToDays(elapsed / 1000);
-          htmlBody += F(" secondes.</p>\n</body>\n</html>");
+          htmlBody += F("</p>\n<p>Ventilation en fonction depuis : ");
+          htmlBody += secondsToDays(fanOnTime / 1000);
+          htmlBody += F("</p>\n</body>\n</html>");
           client.print(htmlBody);
           break;
         }
@@ -187,12 +202,12 @@ float readVoltage(int in){
 }
 
 int setPowerSource(float volts, int out){
-  int power_level = LOW;
+  int powerLevel = LOW;
   if (volts > charged) {
-    power_level = HIGH;
+    powerLevel = HIGH;
   }
-  digitalWrite(out, power_level);
-  return power_level;
+  digitalWrite(out, powerLevel);
+  return powerLevel;
 }
 
 int controlFans(float temp1, float temp2){
@@ -207,19 +222,15 @@ int controlFans(float temp1, float temp2){
 
 int controlExtraFan(float temp1, float temp2){
   int extra_fan = LOW;
-  if (temp1 > extra_hot or temp2 > extra_hot){
+  if (temp1 > extraHot or temp2 > extraHot){
     extra_fan = HIGH;
   }
   digitalWrite(FAN_3_OUT, extra_fan);
   return extra_fan;
 }
+
 void initializeESP8266()
 {
-  // esp8266.begin() verifies that the ESP8266 is operational
-  // and sets it up for the rest of the sketch.
-  // It returns either true or false -- indicating whether
-  // communication was successul or not.
-  // true
   int test = esp8266.begin();
   if (test != true)
   {
@@ -263,10 +274,10 @@ void(* resetFunc) (void) = 0;//declare reset function at address 0
 void errorLoop(int error)
 {
   Serial.print(F("Error: ")); Serial.println(error);
-  Serial.println(F("Will now reboot."));
+  Serial.println(F("Will now reset the arduino."));
   Serial.println();
   delay(3000);
-   resetFunc();  //call reset
+   resetFunc(); 
 }
 
 
@@ -288,7 +299,7 @@ String secondsToDays(long timestamp){
     int jours = ttl_jours % 365;
     int annees = (ttl_jours - jours) / 365;
     int n;
-    char buf[30];
-    n = sprintf(buf, "%d années, %d jours, %02d:%02d:%02d", annees, jours, heures, minutes, secondes);
+    char buf[35];
+    n = sprintf(buf, "%d années, %d jours, %02d h %02d m %02d s", annees, jours, heures, minutes, secondes);
     return buf;
 }
