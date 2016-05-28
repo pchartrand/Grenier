c9d1fc38c6974dae6213d0f551d576868bba7821
Philippe Chartrand <philippe.chartrand@videotron.ca>
2016-05-28 02:39:45 -0400

    mesurer le voltage une seule fois par boucle, extrait le code http dans acceptRequests et le code html dans sendResponse

--- grenier_wifi/grenier_wifi.ino
+++ grenier_wifi/grenier_wifi.ino
@@ -47,10 +47,10 @@ unsigned long elapsed = 0;
 
 long fanOnTime;
 
-int sourceOneStatus;
-int sourceTwoStatus;
+int sourceOneStatus = LOW;
+int sourceTwoStatus = LOW;
 int fanStatus = LOW;
-int extraFanStatus;
+int extraFanStatus = LOW;
 
 float temp1;
 float temp2;
@@ -93,101 +93,30 @@ void setup(){
 void loop(){
   temp1 = readTemp(TEMP_1_IN);
   temp2 = readTemp(TEMP_2_IN);
-  volt1 = readVoltage(VOLTAGE_1_IN);
-  volt2 = readVoltage(VOLTAGE_2_IN);
   leapTime = millis();
   if (leapTime > time + sleepTime){
-    sourceOneStatus = setPowerSource(volt1, SOLAR_1_OUT);
-    sourceTwoStatus = setPowerSource(volt2, SOLAR_2_OUT);
     if (fanStatus == HIGH){
          fanOnTime += (leapTime - time);
     }
     fanStatus = controlFans(temp1, temp2);
     extraFanStatus = controlExtraFan(temp1, temp2);
+    if (fanStatus == HIGH){
+      delay(measurementTime);
+      volt1 = readVoltage(VOLTAGE_1_IN);
+      volt2 = readVoltage(VOLTAGE_2_IN);
+      sourceOneStatus = setPowerSource(volt1, SOLAR_1_OUT);
+      sourceTwoStatus = setPowerSource(volt2, SOLAR_2_OUT);
+    }  
     time = leapTime;
+  }else{
+    volt1 = readVoltage(VOLTAGE_1_IN);
+    volt2 = readVoltage(VOLTAGE_2_IN);
   }
   elapsed = leapTime - startTime;
-  ESP8266Client client = server.available(500);
-  if (client) 
-  {
-    Serial.println(F("Client Connected!"));
-    // an http request ends with a blank line
-    boolean currentLineIsBlank = true;
-    while (client.connected()) 
-    {
-      if (client.available()) 
-      {
-        char c = client.read();
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the http request has ended,
-        // so you can send a reply
-        if (c == '\n' && currentLineIsBlank) 
-        {
-          Serial.println(F("Sending HTML page"));
-          // send a standard http response header:
-          client.print(htmlHeader);
-          String htmlBody;
-          htmlBody  = F("<html>\n<head><title>Grenier</title></head>\n<body><h1>Grenier</h1>\n<p>Température intérieure :");
-          htmlBody += temp1;
-          htmlBody += F("C\n<br />Température extérieure :");
-          htmlBody += temp2;
-          htmlBody += F("C\n</p>\n<p>Tension sur panneau solaire 1 : ");
-          htmlBody += volt1;
-          htmlBody += F("V\n<br />Tension sur panneau solaire 2 : ");
-          htmlBody += volt2;
-          htmlBody += F("V</p>\n");
-          client.print(htmlBody);
-          
-          htmlBody  = F("<p>Ventilateur 1 sur solaire : ");
-          htmlBody += booleanToText(sourceOneStatus);
-          htmlBody += F("<br />\nVentilateur 2 sur solaire : ");
-          htmlBody += booleanToText(sourceTwoStatus);
-          htmlBody += F("</p>\n<p>ventilateurs 1 et 2 en fonction : ");
-          htmlBody += booleanToText(fanStatus);
-          htmlBody += F("<br />\nVentilateur supplémentaire en fonction : ");
-          htmlBody += booleanToText(extraFanStatus);
-          htmlBody += F("</p>\n");
-          client.print(htmlBody);
-          
-          htmlBody  = F("<p>Arduino en fonction depuis : ");
-          htmlBody += secondsToDays(elapsed / 1000);
-          htmlBody += F("</p>\n<p>Ventilation en fonction depuis : ");
-          htmlBody += secondsToDays(fanOnTime / 1000);
-          htmlBody += F("</p>\n</body>\n</html>");
-          client.print(htmlBody);
-          break;
-        }
-        if (c == '\n') 
-        {
-          // you're starting a new line
-          currentLineIsBlank = true;
-        }
-        else if (c != '\r') 
-        {
-          // you've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
-      }
-    }
-    // give the web browser time to receive the data
-    delay(1);
-   
-    // close the connection:
-    client.stop();
-    Serial.println(F("Client disconnected"));
-  }
+  acceptRequests();
 }
 
 /* functions */
-
-float valToVolts(int val){
-  return (val / 1023.0) * referenceVolts * voltageDivider;
-}
-
-float getTemp(int in){
-    float volts = (analogRead(in) / 1023.0) * referenceVolts;
-    return a3*volts*volts*volts + b2*volts*volts + c1*volts + d;
-}
 
 float readTemp(int in){
   float temp = getTemp(in);
@@ -199,6 +128,15 @@ float readVoltage(int in){
   float volts = valToVolts(analogRead(in));
   delay(measurementTime);
   return volts;
+}
+
+float valToVolts(int val){
+  return (val / 1023.0) * referenceVolts * voltageDivider;
+}
+
+float getTemp(int in){
+    float volts = (analogRead(in) / 1023.0) * referenceVolts;
+    return a3*pow(volts, 3) + b2*sq(volts) + c1*volts + d;
 }
 
 int setPowerSource(float volts, int out){
@@ -235,7 +173,7 @@ void initializeESP8266()
   if (test != true)
   {
     Serial.println(F("Error talking to ESP8266."));
-    errorLoop(test);
+    restart(test);
   }
   Serial.println(F("ESP8266 Shield Present"));
 }
@@ -249,7 +187,7 @@ void connectESP8266()
     if (retVal < 0)
     {
       Serial.println(F("Error setting mode."));
-      errorLoop(retVal);
+      restart(retVal);
     }
   }
   Serial.println(F("Mode set to station"));
@@ -264,14 +202,14 @@ void connectESP8266()
     if (retVal < 0)
     {
       Serial.println(F("Error connecting"));
-      errorLoop(retVal);
+      restart(retVal);
     }
   }
 }
 
 void(* resetFunc) (void) = 0;//declare reset function at address 0
 
-void errorLoop(int error)
+void restart(int error)
 {
   Serial.print(F("Error: ")); Serial.println(error);
   Serial.println(F("Will now reset the arduino."));
@@ -280,13 +218,82 @@ void errorLoop(int error)
    resetFunc(); 
 }
 
+void acceptRequests(){
+  ESP8266Client client = server.available(500);
+  if (client) 
+  {
+    Serial.println(F("Client Connected!"));
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) 
+    {
+      if (client.available()) 
+      {
+        char c = client.read();
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) 
+        {
+          Serial.println(F("Sending HTML page"));
+          sendResponse(client);
+          break;
+        }
+        if (c == '\n') 
+        {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r') 
+        {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+   
+    client.stop();
+    Serial.println(F("Client disconnected"));
+  }
+}
+  
+void sendResponse(ESP8266Client client){
+    String htmlBody;
+    client.print(htmlHeader);
+    htmlBody  = F("<html>\n<head><title>Grenier</title></head>\n<body><h1>Grenier</h1>\n<p>Température intérieure :");
+    htmlBody += temp1;
+    htmlBody += F("C\n<br />Température extérieure :");
+    htmlBody += temp2;
+    htmlBody += F("C\n</p>\n<p>Tension sur panneau solaire 1 : ");
+    htmlBody += volt1;
+    htmlBody += F("V\n<br />Tension sur panneau solaire 2 : ");
+    htmlBody += volt2;
+    htmlBody += F("V</p>\n");
+    client.print(htmlBody);
+    
+    htmlBody  = F("<p>Ventilateur 1 sur solaire : ");
+    htmlBody += booleanToText(sourceOneStatus);
+    htmlBody += F("<br />\nVentilateur 2 sur solaire : ");
+    htmlBody += booleanToText(sourceTwoStatus);
+    htmlBody += F("</p>\n<p>ventilateurs 1 et 2 en fonction : ");
+    htmlBody += booleanToText(fanStatus);
+    htmlBody += F("<br />\nVentilateur supplémentaire en fonction : ");
+    htmlBody += booleanToText(extraFanStatus);
+    htmlBody += F("</p>\n");
+    client.print(htmlBody);
+    
+    htmlBody  = F("<p>Arduino en fonction depuis : ");
+    htmlBody += secondsToDays(elapsed / 1000);
+    htmlBody += F("</p>\n<p>Ventilation en fonction depuis : ");
+    htmlBody += secondsToDays(fanOnTime / 1000);
+    htmlBody += F("</p>\n</body>\n</html>");
+    client.print(htmlBody);
+}
 
 String booleanToText(int value){
-  if(value == 1){
-    return "oui";
-  }else{
-    return "non";
-  }
+  return (value == 1) ? "oui": "non";
 }
 
 String secondsToDays(long timestamp){
@@ -303,3 +310,4 @@ String secondsToDays(long timestamp){
     n = sprintf(buf, "%d années, %d jours, %02d h %02d m %02d s", annees, jours, heures, minutes, secondes);
     return buf;
 }
+
